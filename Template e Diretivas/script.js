7a0a46a098621444074352ca05bf6464989cd2d3
Victor Barbosa <vicldb99@gmail.com>
2020-05-18 20:03:08 -0300

    Templates e Diretivas

---
+++ Template e Diretivas/script.js
@@ -0,0 +1,13 @@
+const vm = new Vue(
+  {
+    el: '#app',
+    data: {
+      titulo: "Curso de Javascript",
+      conteudo: "Esse é o conteúdo",
+      lado: 5,
+      comprou: true,
+      href: "https://origamid.com",
+      cor: '#333',
+    }
+  }
+);
