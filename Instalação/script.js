3b7da53bd5b8df68693f66ada842969e84289b39
Victor Barbosa <vicldb99@gmail.com>
2020-05-18 19:39:06 -0300

    Reatividade e Virtual DOM

---
+++ Instalação/script.js
@@ -0,0 +1,13 @@
+const vm = new Vue(
+  {
+    el: '#app',
+    data: {
+      nome: "Victor Barbosa",
+      idade: 21,
+      faculdade: {
+        possui: 'sim',
+        curso: 'Análise e Desenvolvimento de Sistemas'
+      }
+    }
+  }
+);
