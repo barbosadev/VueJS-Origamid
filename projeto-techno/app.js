e2975b3eb6534062b2c2a86ab70866a4f4bf3ad7
Victor Barbosa <vicldb99@gmail.com>
2020-05-30 21:47:42 -0300

    0405

---
+++ projeto-techno/app.js
@@ -0,0 +1,31 @@
+const vm = new Vue({
+  el: '#app',
+  data: {
+    produtos: {},
+    produto: false,
+  },
+  methods: {
+    fetchProdutos() {
+      fetch("./api/produtos.json")
+        .then(r => r.json())
+        .then(r => {
+          this.produtos = r;
+        });
+    },
+    fetchProduto(id) {
+      fetch(`./api/produtos/${id}/dados.json`)
+        .then(r => r.json())
+        .then(r => {
+          this.produto = r;
+        });
+    }
+  },
+  filters: {
+    numeroPreco(valor) {
+      return valor.toLocaleString("pt-BR", { style: "currency", currency: "BRL" });
+    }
+  },
+  created() {
+    this.fetchProdutos();
+  }
+});
