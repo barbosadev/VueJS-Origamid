95965d95dbbed48f96b3fc94b02b31908e980163
Victor Barbosa <vicldb99@gmail.com>
2020-06-09 14:44:16 -0300

    localstorage

--- projeto-techno/app.js
+++ projeto-techno/app.js
@@ -3,6 +3,18 @@ const vm = new Vue({
   data: {
     produtos: {},
     produto: false,
+    carrinho: [],
+  },
+  computed: {
+    carrinhoTotal() {
+      let total = 0;
+      if (this.carrinho.length) {
+        this.carrinho.forEach(item => {
+          total += item.preco;
+        });
+      }
+      return total;
+    }
   },
   methods: {
     fetchProdutos() {
@@ -18,6 +30,31 @@ const vm = new Vue({
         .then(r => {
           this.produto = r;
         });
+    },
+    abrirModal(id) {
+      this.fetchProduto(id);
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+      });
+    },
+    fecharModal({ target, currentTarget }) {
+      if (target === currentTarget) {
+        this.produto = false;
+      }
+    },
+    adicionarItem() {
+      this.produto.estoque--;
+      const { id, nome, preco } = this.produto;
+      this.carrinho.push({ id, nome, preco });
+    },
+    removerItem(index) {
+      this.carrinho.splice(index, 1);
+    },
+    checarLocalStorage() {
+      if (window.localStorage.carrinho) {
+        this.carrinho = JSON.parse(window.localStorage.carrinho);
+      }
     }
   },
   filters: {
@@ -25,7 +62,13 @@ const vm = new Vue({
       return valor.toLocaleString("pt-BR", { style: "currency", currency: "BRL" });
     }
   },
+  watch: {
+    carrinho() {
+      window.localStorage.carrinho = JSON.stringify(this.carrinho);
+    }
+  },
   created() {
     this.fetchProdutos();
+    this.checarLocalStorage();
   }
 });
