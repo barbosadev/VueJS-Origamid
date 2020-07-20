48030e8bf70e9329338a6a3e8fe9e89faf5c4353
Victor Barbosa <vicldb99@gmail.com>
2020-07-20 16:23:19 -0300

    exercicios componentes

--- Componentes/ClimaRio.js
+++ Componentes/ClimaRio.js
@@ -1,8 +1,20 @@
 export default {
   data() {
     return {
-      total: 0
+      dadosTempo: {}
     }
   },
-  template: `<button @click="total++">Somar: {{total}}</button>`
+  template: `<button @click="">Somar: {{}}</button>`,
+  methods: {
+    buscarDadosTempo() {
+      fetch('https://www.metaweather.com/api/location/455825/')
+        .then(res => res.json())
+        .then(res => {
+          this.dadosTempo = res;
+        })
+    }
+  },
+  mounted() {
+    this.buscarDadosTempo();
+  }
 };
